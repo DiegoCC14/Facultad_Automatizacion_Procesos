1ed0a4d88af396f21d73af350e39678c2c90b644
DiegoCC14 <diego.xmen123@gmail.com>
2023-03-07 07:24:22 -0300

    +Multihilo proceso csv

--- Calculo_Vapor_Agua_App/Herramientas_Vapor_Agua/views.py
+++ Calculo_Vapor_Agua_App/Herramientas_Vapor_Agua/views.py
@@ -1,9 +1,11 @@
 from pathlib import Path
-import json , random , string , os , shutil
+import json , random , string , os , shutil , threading , time
 from .services.csv_service import elimina_columna_de_csv
 from .services.descomprecion_file_services import unzip
 from .services.search_files import find_extencion_files_recursively
 import pandas as pd
+
+from concurrent.futures import ThreadPoolExecutor , as_completed
 
 from django.http import HttpResponse , JsonResponse
 from django.shortcuts import render , redirect
@@ -35,13 +37,15 @@ class Analisis_Modificasion_CSV( View ):
 			dir_folder_temp_csv = CURRENT_DIR/f'CSV_TEMP/{name_temp_dir}'
 			os.mkdir( dir_folder_temp_csv ) #Creamos la carpeta
 
-			for file_csv in request.FILES.getlist('archivos'): #myfile is the name of your html file button
-				elimina_columna_de_csv( file_csv , columas_a_eliminar , dir_folder_temp_csv , file_csv.name )
 			
+			cantidad_hilos_maximos = 4
+			with ThreadPoolExecutor( max_workers=cantidad_hilos_maximos ) as executor:
+				for file_csv in request.FILES.getlist('archivos'): #myfile is the name of your html file button
+					executor.submit( elimina_columna_de_csv , file_csv , columas_a_eliminar , dir_folder_temp_csv , file_csv.name )
+
 			shutil.make_archive( dir_folder_temp_csv , 'zip', dir_folder_temp_csv)
 			response = HttpResponse(open(f'{dir_folder_temp_csv}.zip', 'rb'), content_type='application/zip')
 			response['Content-Disposition'] = f'attachment; filename="{name_temp_dir}.zip"'
-			
 
 			# Borramos las carpetas y los comprimidos generados
 			os.remove( f'{dir_folder_temp_csv}.zip' )
@@ -50,21 +54,22 @@ class Analisis_Modificasion_CSV( View ):
 			return response
 
 		elif data_post[ 'tipo_archivo' ] == 'zip_file':
-				
 			file_rar = request.FILES.getlist('archivos')[0] #ZIP subido
 			dir_file_unzip = CURRENT_DIR/f"ZIP_TEMP/{file_rar.name.replace('.','_') + name_temp_dir}"
 			unzip( file_rar , dir_file_unzip ) #Descomprimimos el archivo .ZIP
+
+			cantidad_hilos_maximos = 4
+			with ThreadPoolExecutor( max_workers=cantidad_hilos_maximos ) as executor:
+				for pos , file_csv in enumerate( find_extencion_files_recursively( dir_file_unzip , ".csv" ) ):
+					executor.submit(elimina_columna_de_csv, file_csv , columas_a_eliminar , Path( os.path.dirname( file_csv ) ) , file_csv.name )
 			
-			for file_csv in find_extencion_files_recursively( dir_file_unzip , ".csv" ): #Buscamos los csv dentro de una carpeta
-				elimina_columna_de_csv( file_csv , columas_a_eliminar , Path( os.path.dirname( file_csv ) ) , file_csv.name )
-
 			shutil.make_archive( dir_file_unzip , 'zip', dir_file_unzip)
-
 			response = HttpResponse(open(f'{dir_file_unzip}.zip', 'rb'), content_type='application/zip')
 			response['Content-Disposition'] = f'attachment; filename="{name_temp_dir}.zip"'
-
+			
 			# Borramos las carpetas y los comprimidos generados
 			os.remove( f'{dir_file_unzip}.zip' )
 			shutil.rmtree( dir_file_unzip )
 			# --------------------------------------->>>>>>>>>
+			
 			return response
